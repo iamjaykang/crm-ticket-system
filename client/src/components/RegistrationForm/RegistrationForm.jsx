d6aa81cb1c11db29a55c3b8fc08679fcaeb7a1ca
Jay Kang <qhdl24@gmail.com>
2022-10-26 21:55:29 +1300

    feat: added client sided password validator

--- client/src/components/RegistrationForm/RegistrationForm.jsx
+++ client/src/components/RegistrationForm/RegistrationForm.jsx
@@ -7,13 +7,49 @@ const initialState = {
   confirmPass: "",
 };
 
+const passVerification = {
+  isLengthy: false,
+  hasUpper: false,
+  hasLower: false,
+  hasNumber: false,
+  confirmPass: false,
+};
+
 const RegistrationForm = () => {
   const [newUser, setNewUser] = useState(initialState);
+  const [passwordError, setPasswordError] = useState(passVerification);
 
   const onChangeHandler = (e) => {
     const { name, value } = e.target;
 
     setNewUser({ ...newUser, [name]: value });
+    if (name === "password") {
+      const isLengthy = value.length > 8;
+      const hasUpper = /[A-Z]/.test(value);
+      const hasLower = /[a-z]/.test(value);
+      const hasNumber = /[0-9]/.test(value);
+
+      setPasswordError({
+        ...passwordError,
+        isLengthy,
+        hasUpper,
+        hasLower,
+        hasNumber,
+      });
+    }
+
+    if (name === "confirmPass") {
+      setPasswordError({
+        ...passwordError,
+        confirmPass: newUser.password === value,
+      });
+    }
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(newUser);
   };
 
   console.log(newUser);
@@ -24,7 +60,7 @@ const RegistrationForm = () => {
     <>
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
         <h1 className="font-bold border-b text-2xl p-2">User Registration</h1>
-        <form autoComplete="Off">
+        <form autoComplete="Off" onSubmit={onSubmitHandler}>
           <div className="my-4">
             <label
               className="block text-grey-darker text-sm font-bold mb-2"
@@ -72,7 +108,7 @@ const RegistrationForm = () => {
               value={newUser.password}
               onChange={onChangeHandler}
               type="password"
-              placeholder="******************"
+              placeholder="••••••••"
               required
             />
           </div>
@@ -89,22 +125,50 @@ const RegistrationForm = () => {
               onChange={onChangeHandler}
               value={newUser.confirmPass}
               type="password"
-              placeholder="******************"
+              placeholder="••••••••"
               required
             />
+            {!passwordError.confirmPass && (
+              <div className="text-red-600">Password doesn't match!</div>
+            )}
           </div>
           <div className="mb-6 text-left">
             <ul>
-              <li className="">Min 8 characters</li>
-              <li className="">At least one upper case</li>
-              <li className="">At least one lower case</li>
-              <li className="">At least one one case</li>
+              <li
+                className={
+                  passwordError.isLengthy ? "text-green-600" : "text-red-600"
+                }
+              >
+                • Min 8 characters
+              </li>
+              <li
+                className={
+                  passwordError.hasUpper ? "text-green-600" : "text-red-600"
+                }
+              >
+                • At least one upper case
+              </li>
+              <li
+                className={
+                  passwordError.hasLower ? "text-green-600" : "text-red-600"
+                }
+              >
+                • At least one lower case
+              </li>
+              <li
+                className={
+                  passwordError.hasNumber ? "text-green-600" : "text-red-600"
+                }
+              >
+                • At least one one number
+              </li>
             </ul>
           </div>
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-400 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
               type="submit"
+              disabled={Object.values(passwordError).includes(false)}
             >
               Submit
             </button>
